107633dd82147a5301635e264a35748db945b47d
Lucas Giebler <42948021+Viper4@users.noreply.github.com>
2025-07-15 11:03:52 +0200

    Fixed project and installed opencv

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "mainwindow.h"
-
-#include <QApplication>
+#include "PradoEditor.h"
+#include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    PradoEditor window;
+    window.show();
+    return app.exec();
 }
