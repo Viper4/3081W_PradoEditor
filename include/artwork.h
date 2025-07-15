fca03585ccdbc87cee2c63833f138e4706700e8e
Sarah Wood <sjwood2357@gmail.com>
2025-07-15 18:24:55 +0200

    Update artwork.h

--- include/artwork.h
+++ include/artwork.h
@@ -1,67 +1,46 @@
-
-
-#define PRADO_EDITOR_INTERFACE_H
+#ifndef ARTWORK_H
+#define ARTWORK_H
 #include <string>
-#include <string.h>
+#include <opencv2/core.hpp>
 #include <vector>
 
-struct Artwork {
-    std::string work_title;
+struct Artwork
+{
+    std::string id;
+    cv::Mat image;
+    std::string description;
     std::string work_image_url;
-    std::string author;
-    std::string work_subtitle;
+
+    Artwork() = default;
+    Artwork(const std::string &id, const cv::Mat &img)
+        : id(id), image(img) {}
+};
+
+extern std::vector<Artwork> GlobalGallery;
+#endif
+
+// # work_url: string
+// # work_image_url: string
+// # author: string
+//  # work_title: string
+//  # work_subtitle: string
+//  contains year, dimensions, type
+//  # work_description: string
+
+struct SubtitleData
+{
     std::string year;
-    std::string work_description;
-    int x;
-    int y;
+    std::string medium;
+    std::string dimensions;
 };
 
-//# work_url: string
-//# work_image_url: string
-//# author: string
-//# author_bio: string
-//# author_id: string
-// # author_url: string
-// # work_title: string
-// # work_subtitle: string
-// contains year, dimensions, type
-// # work_exposed: string
-// # work_description: string
-
-
-
-
-
-
-class PradoEditorMobileInterface {
+class PradoEditorMobileInterface
+{
 public:
-
-
-    Artwork:Artwork()
-    {
-        id = "";
-        work_title = "";
-    }
-    // Retrieve list of artworks for the main gallery.
-    //for the main page for scrolling through pieces
     std::vector<Artwork> getArtworkGallery();
-
-    std::vector<Artwork> searchArtworks(const std::string& query);
-
-    void sortArtworks(const std::string& criteria){
-
-    }
-
-    Artwork getArtworkDescription(const std::string& artworkId);
-
-    Artwork applyFilterToImage(const std::string& artworkId);
-
-    void resetImage(const std::string& artworkId);
-
-    void splitSubtitle(const std::string &work_subtitle);
-
-    void editImage(const std::string& artworkId);
-    void cropImage(const std::string& artworkId, int x, int y, int width, int height);
-    void rotateImage(const std::string& artworkId, double angle);
+    void sortArtworks(const std::string &criteria);
+    std::string getArtworkDescription(const std::string &artworkId);
+    SubtitleData splitSubtitle(const std::string &work_subtitle);
 };
 
+#endif
