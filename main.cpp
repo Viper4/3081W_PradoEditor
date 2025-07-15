d6a25022c6f7a4d6fcf827da26073c9aac191723
tarowelches <tacowelches@gmail.com>
2025-07-15 11:38:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 
 int main(int argc, char *argv[])
 {
+    //Tasks for Main
+    // The data will be cleaned, with only paintings that have the attribute of year
+    //1. With this data, we have to first initialize the image cache with 50 or however many image structs
+    //2. Obtain the jpg or png file from these structs and display them
+    //3. Update the image cache based on the index of scroll and continue to display new paintings as the user scrolls
+    //4. On event of a click of an image display the options such as edit, reset, or display description
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
